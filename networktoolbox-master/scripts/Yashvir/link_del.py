5c76d98e930de901bf9b701a1cc797390df5e9ed
Yashvir Sangha <yashvir.sangha@gmail.com>
2022-01-26 18:32:03 +0000

    Added data for topologies w/ 1 link removed

--- networktoolbox-master/scripts/Yashvir/link_del.py
+++ networktoolbox-master/scripts/Yashvir/link_del.py
@@ -12,13 +12,13 @@ if __name__ == "__main__":
     p_list = [0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
     lambda_list = []
     for p in p_list:
-        graph = nx.read_gpickle("/Users/yashvirsangha/Desktop/Third_Year_Project/Code_Robin/networktoolbox-master/scripts/Yashvir/Data/graph_{}_{}.gpickle".format(nodes,p))
+        graph = nx.read_gpickle("/Users/yashvirsangha/Desktop/Third_Year_Project/TYP_Code/networktoolbox-master/scripts/Yashvir/Data/graph_{}_{}.gpickle".format(nodes,p))
         graph = nx.relabel.convert_node_labels_to_integers(graph, first_label=1)
 
         remove = random.sample(graph.edges(), link_remove)
         graph.remove_edges_from(remove)\
 
-        nx.write_gpickle(graph, "/Users/yashvirsangha/Desktop/Third_Year_Project/Code_Robin/networktoolbox-master/scripts/Yashvir/Data/graph_{}-{}_{}.gpickle".format(nodes,link_remove,p))
+        nx.write_gpickle(graph, "/Users/yashvirsangha/Desktop/Third_Year_Project/TYP_Code/networktoolbox-master/scripts/Yashvir/Data/graph_{}-{}_{}.gpickle".format(nodes,link_remove,p))
 
 
 
