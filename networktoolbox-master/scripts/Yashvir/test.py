9bbd64c56930ef9e69d4d83928e33e36207b89d3
Yashvir Sangha <yashvir.sangha@gmail.com>
2022-01-26 19:33:34 +0000

    Changed test.py to include removed links 1-7

--- networktoolbox-master/scripts/Yashvir/test.py
+++ networktoolbox-master/scripts/Yashvir/test.py
@@ -10,23 +10,24 @@ if __name__ == "__main__":
     p_list = [0.3,0.4,0.5,0.6,0.7,0.8,0.9]
     lambda_list = []
     for p in p_list:
-        graph = nx.read_gpickle("/Users/yashvirsangha/Desktop/Third_Year_Project/Code_Robin/networktoolbox-master/scripts/Yashvir/Data/graph_{}-1_{}.gpickle".format(nodes,p))
-        graph = nx.relabel.convert_node_labels_to_integers(graph, first_label=1)
-        assert type(graph) == nx.classes.graph.Graph
-        network = nt.Network.OpticalNetwork(graph)
-        print("starting wavelength requirement ILP")
-        time_start = time.perf_counter()
-        
+        for i in range(1,8):
+            graph = nx.read_gpickle("/Users/yashvirsangha/Desktop/Third_Year_Project/Code_Robin/networktoolbox-master/scripts/Yashvir/Data/graph_{}-{}_{}.gpickle".format(nodes,i,p))
+            graph = nx.relabel.convert_node_labels_to_integers(graph, first_label=1)
+            assert type(graph) == nx.classes.graph.Graph
+            network = nt.Network.OpticalNetwork(graph)
+            print("starting wavelength requirement ILP")
+            time_start = time.perf_counter()
+            
 
-        data = network.rwa.static_ILP(min_wave=True, max_time=3600, e=20, k=20, threads=2,
-                                      node_file_start=0.01)
-        time_taken = time.perf_counter()-time_start
-        print("time: {}s".format(time_taken))
-        lambda_list.append((graph.number_of_edges(), data["objective"], data["status"], time_taken))
-    with open("/Users/yashvirsangha/Desktop/Third_Year_Project/Code_Robin/networktoolbox-master/scripts/Yashvir/Data/ILP-1-results.txt", 'w') as f:
-        f.write("E \t lambda \t status \t time taken\n")
-        for E, objective, status, time in lambda_list:
+            data = network.rwa.static_ILP(min_wave=True, max_time=3600, e=20, k=20, threads=2,
+                                        node_file_start=0.01)
+            time_taken = time.perf_counter()-time_start
+            print("time: {}s".format(time_taken))
+            lambda_list.append((graph.number_of_edges(), data["objective"], data["status"], time_taken))
+        with open("/Users/yashvirsangha/Desktop/Third_Year_Project/Code_Robin/networktoolbox-master/scripts/Yashvir/Data/ILP-{}-results.txt".format(i), 'w') as f:
+            f.write("E \t lambda \t status \t time taken\n")
+            for E, objective, status, time in lambda_list:
 
-            f.write("{} \t {} \t {} \t {}".format(E, objective, status, time))
-            f.write("\n")
+                f.write("{} \t {} \t {} \t {}".format(E, objective, status, time))
+                f.write("\n")
 
