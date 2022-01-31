1aa7af9eeb8254b5c223a6158644f22af4be4e87
Yashvir Sangha <yashvir.sangha@gmail.com>
2022-01-31 14:36:42 +0000

    path fix

--- networktoolbox-master/scripts/Yashvir/test.py
+++ networktoolbox-master/scripts/Yashvir/test.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     p_list = [0.3, 0.4,0.5, 0.6, 0.7, 0.8, 0.9]
     lambda_list = []
     for p in p_list:
-        graph = nx.read_gpickle("/home/zceeysa/TYP_Code/networktoolbox/scripts/Yashvir/Data/graph_{}_{}.gpickle".format(nodes,p))
+        graph = nx.read_gpickle("/home/zceeysa/Desktop/TYP_Code/networktoolbox/scripts/Yashvir/Data/graph_{}_{}.gpickle".format(nodes,p))
         graph = nx.relabel.convert_node_labels_to_integers(graph, first_label=1)
         assert type(graph) == nx.classes.graph.Graph
         network = nt.Network.OpticalNetwork(graph)
@@ -22,7 +22,7 @@ if __name__ == "__main__":
         time_taken = time.perf_counter()-time_start
         print("time: {}s".format(time_taken))
         lambda_list.append((graph.number_of_edges(), data["objective"], data["status"], time_taken))
-    with open("/home/zcceysa/TYP_Code/networktoolbox/scripts/Yashvir/Data/ILP-results.txt", 'w') as f:
+    with open("/home/zceeysa/Desktop/TYP_Code/networktoolbox/scripts/Yashvir/Data/ILP-results.txt", 'w') as f:
         f.write("E \t lambda \t status \t time taken\n")
         for E, objective, status, time in lambda_list:
 
