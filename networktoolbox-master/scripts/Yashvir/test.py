1e3e3e6c818b2c6b41e51585467f9907290c766e
Yashvir Sangha <yashvir.sangha@gmail.com>
2022-01-31 14:00:35 +0000

    Doing individual files

--- networktoolbox-master/scripts/Yashvir/test.py
+++ networktoolbox-master/scripts/Yashvir/test.py
@@ -6,9 +6,10 @@ if __name__ == "__main__":
     # read graph
     # create T_c
     # run ILP
-    nodes=15
+    nodes=20
     p_list = [0.3, 0.4,0.5, 0.6, 0.7, 0.8, 0.9]
     lambda_list = []
+    
     for p in p_list:
         graph = nx.read_gpickle("../TYP_code/networktoolbox-master/scripts/Yashvir/Data/graph_{}_{}.gpickle".format(nodes,p))
         graph = nx.relabel.convert_node_labels_to_integers(graph, first_label=1)
@@ -17,7 +18,7 @@ if __name__ == "__main__":
         print("starting wavelength requirement ILP")
         time_start = time.perf_counter()
 
-        data = network.rwa.static_ILP(min_wave=True, max_time=3600, e=20, k=20, threads=80,
+        data = network.rwa.static_ILP(min_wave=True, max_time=60, e=20, k=20, threads=80,
                                       node_file_start=0.01)
         time_taken = time.perf_counter()-time_start
         print("time: {}s".format(time_taken))
