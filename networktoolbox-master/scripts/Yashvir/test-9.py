26bcbbbf8ce3054779159534733240a7cd03500b
Yashvir Sangha <yashvir.sangha@gmail.com>
2022-02-28 00:39:25 +0000

    Fixed pathing issue

--- networktoolbox-master/scripts/Yashvir/test-9.py
+++ networktoolbox-master/scripts/Yashvir/test-9.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     p_list = [0.3]
     lambda_list = []
     for i in range(0, 14, 2):
-        graph = nx.read_gpickle("networktoolbox-master/scripts/Yashvir/BA_data/15(3)-{}_3.gpickle".format(i))
+        graph = nx.read_gpickle("/home/zceeysa/Desktop/TYP_Code/networktoolbox-master/scripts/Yashvir/BA_data/15(3)-{}_3.gpickle".format(i))
         graph = nx.relabel.convert_node_labels_to_integers(graph, first_label=1)
         assert type(graph) == nx.classes.graph.Graph
         network = nt.Network.OpticalNetwork(graph)
@@ -24,7 +24,7 @@ if __name__ == "__main__":
         lambda_list.append((graph.number_of_edges(), data["objective"], data["status"], time_taken))
 
 
-    with open("networktoolbox-master/scripts/Yashvir/BA_data/ILP-results-3.txt", 'w') as f:
+    with open("/home/zceeysa/Desktop/TYP_Code/networktoolbox-master/scripts/Yashvir/BA_data/ILP-results-3.txt", 'w') as f:
         f.write("E \t lambda \t status \t time taken\n")
         for E, objective, status, time in lambda_list:
 
