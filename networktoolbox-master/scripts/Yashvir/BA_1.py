4d399921de2f5ed25eea9884fc492288dd857dec
Yashvir Sangha <yashvir.sangha@gmail.com>
2022-03-09 14:40:54 +0000

    Forgot BA 8

--- networktoolbox-master/scripts/Yashvir/BA_1.py
+++ networktoolbox-master/scripts/Yashvir/BA_1.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     p_list = [0.342]
     lambda_list = []
     for i in range(0, 14, 2):
-        graph = nx.read_gpickle("/home/zceeysa/Desktop/TYP_Code/networktoolbox-master/scripts/Yashvir/15_36_BA_data/15(6)-{}_3.gpickle".format(i))
+        graph = nx.read_gpickle("/home/zceeysa/Desktop/TYP_Code/networktoolbox-master/scripts/Yashvir/15_36_BA_data/15(8)-{}_3.gpickle".format(i))
         graph = nx.relabel.convert_node_labels_to_integers(graph, first_label=1)
         assert type(graph) == nx.classes.graph.Graph
         network = nt.Network.OpticalNetwork(graph)
@@ -25,7 +25,7 @@ if __name__ == "__main__":
         lambda_list.append((graph.number_of_edges(), data["objective"], data["status"], time_taken))
 
 
-    with open("/home/zceeysa/Desktop/TYP_Code/networktoolbox-master/scripts/Yashvir/15_36_BA_data/ILP-results-6.txt", 'w') as f:
+    with open("/home/zceeysa/Desktop/TYP_Code/networktoolbox-master/scripts/Yashvir/15_36_BA_data/ILP-results-8.txt", 'w') as f:
         f.write("E \t lambda \t status \t time taken\n")
         for E, objective, status, time in lambda_list:
 
