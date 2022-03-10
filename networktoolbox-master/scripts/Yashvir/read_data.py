afba09b33ebf7c78fb3d048f1c1e371c198eaa2e
Yashvir Sangha <yashvir.sangha@gmail.com>
2022-03-10 19:00:33 +0000

    Finished data read

--- networktoolbox-master/scripts/Yashvir/read_data.py
+++ networktoolbox-master/scripts/Yashvir/read_data.py
@@ -1,21 +1,23 @@
+import numpy as np
+
+for i in range(1, 11):
+
+    with open("../TYP_code/networktoolbox-master/scripts/Yashvir/15_36_BA_data/ILP-results-{}.txt".format(i)) as f:
+        lines = f.readlines()[1:]
 
 
+        lambda_list = []
+        edge_list = []
 
-with open("../TYP_code/networktoolbox-master/scripts/Yashvir/15_36_BA_data/ILP-results-1.txt") as f:
-    lines = f.readlines()[1:]
+        for line in lines:
+            edge = float(line[0:line.index(" ")])
+            edge_list.append(edge)
+            line = line[line.index(" ")+3:]
+            lam = float(line[0:line.index(" ")])
+            lambda_list.append(lam)
 
-count = 0
-
-lamda = []
-edges = []
-
-for line in lines:
-    count += 1 
-    edges.append(line[0:line.index(" ")])
-    line = line[line.index(" ")+3:]
-    lamda.append(line[0:line.index(" ")])
-
-
+        slope, intercept = np.polyfit(edge_list, lambda_list,1)
+        print(slope)
 
 
 
