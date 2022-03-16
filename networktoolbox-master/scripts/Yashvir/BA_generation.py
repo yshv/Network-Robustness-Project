d2cc7daa8b01e53c84451d7e36f00482d436300b
Yashvir Sangha <yashvir.sangha@gmail.com>
2022-03-16 15:15:10 +0000

    New scripts

---
+++ networktoolbox-master/scripts/Yashvir/BA_generation.py
@@ -0,0 +1,46 @@
+import NetworkToolkit as nt
+import networkx as nx
+import random
+from datetime import datetime
+import ray
+import copy
+
+@ray.remote
+def ba_generation_distributed(nodes, m, edge_step, edge_count, graph_num):
+    graph = ba_generation(nodes, m)
+    nt.Database.insert_graph(graph, "Topology_Data", "robustness-sim-test", node_data=True, use_pickle=True, type="BA",
+                             timestamp=datetime.utcnow(), graph_num=graph_num, edge_removal_count=edge_count, edge_removal_step=edge_step,
+                             BA_m=m, edge_removal_idx=0)
+    for i in range(1, edge_count+1):
+        edges = list(graph.edges)
+        choices = random.sample(edges, edge_step)
+        graph_copy = copy.deepcopy(graph)
+        graph_copy.remove_edges_from(choices)
+        while nx.is_connected(graph_copy) != True or min([d for n, d in graph_copy.degree()])<=2:
+            choices = random.sample(edges, edge_step)
+            graph_copy = copy.deepcopy(graph)
+            graph_copy.remove_edges_from(choices)
+        nt.Database.insert_graph(graph_copy, "Topology_Data", "robustness-sim-test", node_data=True, use_pickle=True, type="BA",
+                                 timestamp=datetime.utcnow(), graph_num=graph_num, edge_removal_count=edge_count, edge_removal_step=edge_step,
+                                 BA_m=m, edge_removal_idx=i)
+        graph = graph_copy
+
+
+
+
+def ba_generation(nodes, m):
+    graph = nx.barabasi_albert_graph(nodes, m)
+    while nx.is_connected(graph) != True or min([d for n, d in graph.degree()])<=2:
+        graph = nx.barabasi_albert_graph(nodes, m)
+    return graph
+
+if __name__ == "__main__":
+    nodes = 15
+    graph_num = 10
+    edge_step = 2
+    edge_count = 10
+    ray.init()
+    tasks = [ba_generation_distributed.remote(nodes, 3, edge_step, edge_count, i) for i in range(0, graph_num)]
+    ray.get(tasks)
+
+
---
+++ networktoolbox-master/scripts/Yashvir/throughput_simulations.py
@@ -0,0 +1,41 @@
+import NetworkToolkit as nt
+import time
+from datetime import datetime
+import ray
+import networkx as nx
+num_cpus = 1
+
+
+@ray.remote(num_cpus=num_cpus)
+def static_ilp_distributed(graph, _id, max_time=3600, e=20, k=20, threads=1, node_file_start=0.01,
+                           db=None, collection=None, actor=None):
+    graph = nx.relabel.convert_node_labels_to_integers(graph, first_label=1)
+    network = nt.Network.OpticalNetwork(graph, channel_bandwidth=170*1e9)
+    print("nodes: {}".format(graph.nodes))
+    time_start = time.perf_counter()
+    data = network.rwa.static_ILP(min_wave=True, max_time=max_time, e=e, k=k, threads=threads,
+                                  node_file_start=node_file_start)
+    time_taken = time.perf_counter()-time_start
+    nt.Database.update_data_with_id(db, collection, _id, newvals={"$set": {"lambda_r":data["objective"],
+                                                                           "lambda_r optimisation status": str(data["status"]),
+                                                                           "lambda_r time": time_taken,
+                                                                           "lambda_r max time":max_time,
+                                                                           "lambda_r e": e,
+                                                                           "lambda_r k": k,
+                                                                           "lambda_r threads":threads,
+                                                                           "lambda_r node_file_start":node_file_start,
+                                                                           "lambda_r timestamp": datetime.utcnow()}})
+    if actor is not None:
+        actor.update.remote(1)
+if __name__== "__main__":
+
+    hostname = "128.40.41.48"
+    port = 7112
+    # ray.init(address='{}:{}'.format(hostname, port), _redis_password='5241590000000000', ignore_reinit_error=True)
+    ray.init()
+    graph_list = nt.Database.read_topology_dataset_list("Topology_Data", "robustness-sim-test", find_dic={"lambda_r":{"$exists":False}})
+    pb = nt.Tools.ProgressBar(len(graph_list))
+    actor = pb.actor
+    tasks = [static_ilp_distributed.remote(graph, _id, db="Topology_Data", collection="robustness-sim-test", actor=actor, max_time=3600, threads=num_cpus) for graph, _id in graph_list]
+    pb.print_until_done()
+    ray.get(tasks)
