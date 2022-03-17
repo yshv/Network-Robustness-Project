f8826a47d32f175fa4998ebd7a4fff98b327f960
Yashvir Sangha <zceeysa@lyon.ee.ucl.ac.uk>
2022-03-17 23:34:30 +0000

    finally finished testing

--- networktoolbox-master/scripts/Yashvir/BA_generation.py
+++ networktoolbox-master/scripts/Yashvir/BA_generation.py
@@ -8,7 +8,7 @@ import copy
 @ray.remote
 def ba_generation_distributed(nodes, m, edge_step, edge_count, graph_num):
     graph = ba_generation(nodes, m)
-    nt.Database.insert_graph(graph, "Topology_Data", "robustness-sim", node_data=True, use_pickle=True, type="BA",
+    nt.Database.insert_graph(graph, "Topology_Data", "robustness-sim-test", node_data=True, use_pickle=True, type="BA",
                              timestamp=datetime.utcnow(), graph_num=graph_num, edge_removal_count=edge_count, edge_removal_step=edge_step,
                              BA_m=m, edge_removal_idx=0)
     for i in range(1, edge_count+1):
@@ -20,7 +20,7 @@ def ba_generation_distributed(nodes, m, edge_step, edge_count, graph_num):
             choices = random.sample(edges, edge_step)
             graph_copy = copy.deepcopy(graph)
             graph_copy.remove_edges_from(choices)
-        nt.Database.insert_graph(graph_copy, "Topology_Data", "robustness-sim", node_data=True, use_pickle=True, type="BA",
+        nt.Database.insert_graph(graph_copy, "Topology_Data", "robustness-sim-test", node_data=True, use_pickle=True, type="BA",
                                  timestamp=datetime.utcnow(), graph_num=graph_num, edge_removal_count=edge_count, edge_removal_step=edge_step,
                                  BA_m=m, edge_removal_idx=i)
         graph = graph_copy
@@ -36,7 +36,7 @@ def ba_generation(nodes, m):
 
 if __name__ == "__main__":
     nodes = 15
-    graph_num = 200
+    graph_num = 500
     edge_step = 2
     edge_count = 10
     ray.init()
