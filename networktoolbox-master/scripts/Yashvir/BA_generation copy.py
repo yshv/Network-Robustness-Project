67df06a3b63ec89f59dd15818e7462bce40c65fa
Yashvir Sangha <yashvir.sangha@gmail.com>
2022-03-17 16:11:39 +0000

    Testing ER generation

--- networktoolbox-master/scripts/Yashvir/BA_generation copy.py
+++ networktoolbox-master/scripts/Yashvir/BA_generation copy.py
@@ -5,11 +5,11 @@ from datetime import datetime
 import ray
 import copy
 
-def er_generation_distributed(nodes, p, edge_step, edge_count, graph_num):
-    graph = er_generation(nodes, p)
-    nt.Database.insert_graph(graph, "Topology_Data", "robustness-sim-test-test", node_data=True, use_pickle=True, type="ER",
+def ba_generation_distributed(nodes, m, edge_step, edge_count, graph_num):
+    graph = ba_generation(nodes, m)
+    nt.Database.insert_graph(graph, "Topology_Data", "robustness-sim-test-test", node_data=True, use_pickle=True, type="BA",
                              timestamp=datetime.utcnow(), graph_num=graph_num, edge_removal_count=edge_count, edge_removal_step=edge_step,
-                             ER_p=p, edge_removal_idx=0)
+                             BA_m=m, edge_removal_idx=0)
     for i in range(1, edge_count+1):
         edges = list(graph.edges)
         choices = random.sample(edges, edge_step)
@@ -19,25 +19,26 @@ def er_generation_distributed(nodes, p, edge_step, edge_count, graph_num):
             choices = random.sample(edges, edge_step)
             graph_copy = copy.deepcopy(graph)
             graph_copy.remove_edges_from(choices)
-        nt.Database.insert_graph(graph_copy, "Topology_Data", "robustness-sim-test-test", node_data=True, use_pickle=True, type="ER",
+        nt.Database.insert_graph(graph_copy, "Topology_Data", "robustness-sim-test-test", node_data=True, use_pickle=True, type="BA",
                                  timestamp=datetime.utcnow(), graph_num=graph_num, edge_removal_count=edge_count, edge_removal_step=edge_step,
-                                 ER_p=p, edge_removal_idx=i)
+                                 BA_m=m, edge_removal_idx=i)
         graph = graph_copy
 
-def er_generation(nodes, p):
-    graph = nx.erdos_renyi_graph(nodes, p)
-    num_edges = graph.number_of_edges()
-    while nx.is_connected(graph) != True or min([d for n, d in graph.degree()])<=2 or num_edges != 50:
-        graph = nx.erdos_renyi_graph(nodes, p)
+
+
+
+def ba_generation(nodes, m):
+    graph = nx.barabasi_albert_graph(nodes, m)
+    while nx.is_connected(graph) != True or min([d for n, d in graph.degree()])<=2:
+        graph = nx.barabasi_albert_graph(nodes, m)
     return graph
 
 if __name__ == "__main__":
     nodes = 15
-    graph_num = 20
+    graph_num = 10
     edge_step = 2
     edge_count = 10
     for i in range(0, graph_num):
-        er_generation_distributed(nodes, 0.45, edge_step, edge_count, i)
+        ba_generation_distributed(nodes, 5, edge_step, edge_count, i)
         print(i)
-
 
