ea39019f03733b199048214219d6bee85eb68711
Yashvir Sangha <zceeysa@vm-berlin.ee.ucl.ac.uk>
2022-03-18 15:22:55 +0000

    failed scripting

--- networktoolbox-master/scripts/Yashvir/throughput_simulations.py
+++ networktoolbox-master/scripts/Yashvir/throughput_simulations.py
@@ -28,7 +28,7 @@ def static_ilp_distributed(graph, _id, max_time=3600, e=20, k=20, threads=1, nod
                                                                            "lambda_r k": k,
                                                                            "lambda_r threads":threads,
                                                                            "lambda_r node_file_start":node_file_start,
-                                                                           "lambda_r timestamp": datetime.utcnow()
+                                                                           "lambda_r timestamp": datetime.utcnow(),
                                                                            "edge conn": connectivity,
                                                                            "diamter": diamter,
                                                                            "algebraic connectivity": alge_con,
@@ -42,9 +42,9 @@ if __name__== "__main__":
     port = 7112
     # ray.init(address='{}:{}'.format(hostname, port), _redis_password='5241590000000000', ignore_reinit_error=True)
     ray.init()
-    graph_list = nt.Database.read_topology_dataset_list("Topology_Data", "robustness-sim-test", find_dic={"lambda_r":{"$exists":False}})
+    graph_list = nt.Database.read_topology_dataset_list("Topology_Data", "robustness-sim-test-test", find_dic={"lambda_r":{"$exists":False}})
     pb = nt.Tools.ProgressBar(len(graph_list))
     actor = pb.actor
-    tasks = [static_ilp_distributed.remote(graph, _id, db="Topology_Data", collection="robustness-sim-test", actor=actor, max_time=3600, threads=num_cpus) for graph, _id in graph_list]
+    tasks = [static_ilp_distributed.remote(graph, _id, db="Topology_Data", collection="robustness-sim-test-test", actor=actor, max_time=3600, threads=num_cpus) for graph, _id in graph_list]
     pb.print_until_done()
     ray.get(tasks)
