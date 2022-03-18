55096653ab84cd16aaec1a0aa7ad7745ecaf3b19
Yashvir Sangha <yashvir.sangha@gmail.com>
2022-03-18 15:55:16 +0000

    Testing without ray

--- networktoolbox-master/scripts/Yashvir/throughput_simulations_mine.py
+++ networktoolbox-master/scripts/Yashvir/throughput_simulations_mine.py
@@ -6,7 +6,6 @@ import networkx as nx
 num_cpus = 1
 
 
-@ray.remote(num_cpus=num_cpus)
 def static_ilp_distributed(graph, _id, max_time=3600, e=20, k=20, threads=1, node_file_start=0.01,
                            db=None, collection=None, actor=None):
     graph = nx.relabel.convert_node_labels_to_integers(graph, first_label=1)
@@ -41,10 +40,7 @@ if __name__== "__main__":
     hostname = "128.40.41.48"
     port = 7112
     # ray.init(address='{}:{}'.format(hostname, port), _redis_password='5241590000000000', ignore_reinit_error=True)
-    ray.init()
     graph_list = nt.Database.read_topology_dataset_list("Topology_Data", "robustness-sim-test", find_dic={"lambda_r":{"$exists":False}})
-    pb = nt.Tools.ProgressBar(len(graph_list))
-    actor = pb.actor
-    tasks = [static_ilp_distributed.remote(graph, _id, db="Topology_Data", collection="robustness-sim-test", actor=actor, max_time=3600, threads=num_cpus) for graph, _id in graph_list]
-    pb.print_until_done()
-    ray.get(tasks)
+    for graph, _id in graph_list:
+        static_ilp_distributed(graph, _id, db="Topology_Data", collection="robustness-sim-test", max_time=3600, threads=num_cpus)
+
