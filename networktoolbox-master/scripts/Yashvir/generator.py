2399c8cb6d8950c2b1e7e7a4de37ba59ea01fa17
Yashvir Sangha <yashvir.sangha@gmail.com>
2022-03-24 21:43:19 +0000

    Created script to generate all possible topologies

---
+++ networktoolbox-master/scripts/Yashvir/generator.py
@@ -0,0 +1,101 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+import NetworkToolkit as nt
+import random
+from datetime import datetime
+import ray
+import copy
+
+
+def plot_graphs(graphs, figsize=14, dotsize=20):
+    """Utility to plot a lot of graphs from an array of graphs. 
+    Each graphs is a list of edges; each edge is a tuple."""
+    n = len(graphs)
+    fig = plt.figure(figsize=(figsize,figsize))
+    fig.patch.set_facecolor('white') # To make copying possible (no transparent background)
+    k = int(np.sqrt(n))
+    for i in range(n):
+        plt.subplot(k+1,k+1,i+1)
+        g = nx.Graph()
+        for e in graphs[i]:            
+            g.add_edge(e[0],e[1])
+        nt.Database.insert_graph(graph, "Topology_Data", "robustness-sim-basic", node_data=True, use_pickle=True, type="basic",
+                            timestamp=datetime.utcnow())
+
+def make_graphs(n=2, i=None, j=None):
+    """Make a graph recursively, by either including, or skipping each edge.
+    Edges are given in lexicographical order by construction."""
+    out = []
+    if i is None: # First call
+        out  = [[(0,1)]+r for r in make_graphs(n=n, i=0, j=1)]
+    elif j<n-1:
+        out += [[(i,j+1)]+r for r in make_graphs(n=n, i=i, j=j+1)]
+        out += [          r for r in make_graphs(n=n, i=i, j=j+1)]
+    elif i<n-1:
+        out = make_graphs(n=n, i=i+1, j=i+1)
+    else:
+        out = [[]]
+    return out
+
+def perm(n, s=None):
+    """All permutations of n elements."""
+    if s is None: return perm(n, tuple(range(n)))
+    if not s: return [[]]
+    return [[i]+p for i in s for p in perm(n, tuple([k for k in s if k!=i]))]
+
+def permute(g, n):
+    """Create a set of all possible isomorphic codes for a graph, 
+    as nice hashable tuples. All edges are i<j, and sorted lexicographically."""
+    ps = perm(n)
+    out = set([])
+    for p in ps:
+        out.add(tuple(sorted([(p[i],p[j]) if p[i]<p[j] else (p[j],p[i]) for i,j in g])))
+    return list(out)
+
+
+def connected(g):
+    """Check if the graph is fully connected, with Union-Find."""
+    nodes = set([i for e in g for i in e])
+    roots = {node: node for node in nodes}
+    
+    def _root(node, depth=0):
+        if node==roots[node]: return (node, depth)
+        else: return _root(roots[node], depth+1)
+    
+    for i,j in g:
+        ri,di = _root(i)
+        rj,dj = _root(j)
+        if ri==rj: continue
+        if di<=dj: roots[ri] = rj
+        else:      roots[rj] = ri
+    return len(set([_root(node)[0] for node in nodes]))==1
+
+assert connected([(0, 1), (0, 2), (1, 3)])
+assert not connected([(0, 1), (2, 3)]) 
+
+
+def filter(gs, target_nv):
+    """Filter all improper graphs: those with not enough nodes, 
+    those not fully connected, and those isomorphic to previously considered."""
+    mem = set({})
+    gs2 = []
+    for g in gs:
+        nv = len(set([i for e in g for i in e]))
+        if nv != target_nv:
+            continue
+        if not connected(g):
+            continue
+        if tuple(g) not in mem:
+            gs2.append(g)
+            mem |= set(permute(g, target_nv))
+        #print('\n'.join([str(a) for a in mem]))
+    return gs2
+
+NV = 4
+print('Building...')
+gs = make_graphs(NV)
+print('Filtering...')
+gs = filter(gs, NV)
+print(f'Drawing {len(gs)} graphs...')
+plot_graphs(gs, figsize=14, dotsize=20)
