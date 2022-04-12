7004d654e2cb453dcf0829c1ccc6acc549c57549
Yashvir Sangha <yashvir.sangha@gmail.com>
2022-04-12 16:35:37 +0100

    21 edges

--- networktoolbox-master/scripts/Yashvir/BA_generation.py
+++ networktoolbox-master/scripts/Yashvir/BA_generation.py
@@ -20,7 +20,7 @@ def ba_generation_distributed(nodes, m, edge_step, edge_count, graph_num):
             choices = random.sample(edges, edge_step)
             graph_copy = copy.deepcopy(graph)
             graph_copy.remove_edges_from(choices)
-        nt.Database.insert_graph(graph_copy, "Topology_Data", "robustness-sim-test-test", node_data=True, use_pickle=True, type="BA",
+        nt.Database.insert_graph(graph_copy, "Topology_Data", "robustness-sim-21", node_data=True, use_pickle=True, type="BA",
                                  timestamp=datetime.utcnow(), graph_num=graph_num, edge_removal_count=edge_count, edge_removal_step=edge_step,
                                  BA_m=m, edge_removal_idx=i)
         graph = graph_copy
@@ -35,12 +35,12 @@ def ba_generation(nodes, m):
     return graph
 
 if __name__ == "__main__":
-    nodes = 15
-    graph_num = 500
+    nodes = 10
+    graph_num = 100
     edge_step = 2
-    edge_count = 10
+    edge_count = 3
     ray.init()
-    tasks = [ba_generation_distributed.remote(nodes, 5, edge_step, edge_count, i) for i in range(0, graph_num)]
+    tasks = [ba_generation_distributed.remote(nodes, 3, edge_step, edge_count, i) for i in range(0, graph_num)]
     ray.get(tasks)
 
 
--- networktoolbox-master/scripts/Yashvir/ER_generation.py
+++ networktoolbox-master/scripts/Yashvir/ER_generation.py
@@ -20,7 +20,7 @@ def ba_generation_distributed(nodes, p, edge_step, edge_count, graph_num):
             choices = random.sample(edges, edge_step)
             graph_copy = copy.deepcopy(graph)
             graph_copy.remove_edges_from(choices)
-        nt.Database.insert_graph(graph_copy, "Topology_Data", "robustness-sim-test-test", node_data=True, use_pickle=True, type="ER",
+        nt.Database.insert_graph(graph_copy, "Topology_Data", "robustness-sim-21", node_data=True, use_pickle=True, type="ER",
                                  timestamp=datetime.utcnow(), graph_num=graph_num, edge_removal_count=edge_count, edge_removal_step=edge_step,
                                  ER_p=p, edge_removal_idx=i)
         graph = graph_copy
@@ -31,14 +31,14 @@ def ba_generation_distributed(nodes, p, edge_step, edge_count, graph_num):
 def ba_generation(nodes, p):
     graph = nx.erdos_renyi_graph(nodes, p)
     num_edges = graph.number_of_edges()
-    while nx.is_connected(graph) != True or min([d for n, d in graph.degree()])<=2 or num_edges != 50:
+    while nx.is_connected(graph) != True or min([d for n, d in graph.degree()])<=2 or num_edges != 21:
         graph = nx.erdos_renyi_graph(nodes, p)
         num_edges = graph.number_of_edges()
     return graph
 
 if __name__ == "__main__":
-    nodes = 15
-    graph_num = 2
+    nodes = 10
+    graph_num = 100
     edge_step = 2
     edge_count = 10
     ray.init()
